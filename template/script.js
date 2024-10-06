f1cc9f4e8cac6eb4d3569f5992a654a3540db98b
cht-au <145236495+cht-au@users.noreply.github.com>
2024-10-06 17:43:07 -0400

    implement changing color button

--- template/script.js
+++ template/script.js
@@ -1,7 +1,15 @@
-/*
-Template format for a listener
+const button = document.querySelector("button");
+const colorBar = document.getElementById('colorBar');
 
-const <Button> = document.getElementById('id');
-<Button>.addEventListener('input', function() {
+button.addEventListener("click", (event) => {
+    colorBar.style.background = getRandomColor();
 });
-*/
+
+function getRandomColor() {
+    var letters = '0123456789ABCDEF';
+    var color = '#';
+    for (var i = 0; i < 6; i++) {
+      color += letters[Math.floor(Math.random() * 16)]; 
+    }
+    return color;
+  }
