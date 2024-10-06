3a237af0381a63d192229d82ca3c8ee5d50c32eb
cht-au <145236495+cht-au@users.noreply.github.com>
2024-10-06 17:51:46 -0400

    fix folder location

--- template/script.js
+++ template/script.js
@@ -1,15 +1,22 @@
-const button = document.querySelector("button");
-const colorBar = document.getElementById('colorBar');
+/*
+Template format for a listener
 
-button.addEventListener("click", (event) => {
-    colorBar.style.background = getRandomColor();
+const <Button> = document.getElementById('id');
+<Button>.addEventListener('input', function() {
 });
+*/
 
-function getRandomColor() {
-    var letters = '0123456789ABCDEF';
-    var color = '#';
-    for (var i = 0; i < 6; i++) {
-      color += letters[Math.floor(Math.random() * 16)]; 
-    }
-    return color;
-  }
+let count = 0;
+
+const counterDisplay = document.getElementById('counterBox');
+const incrementButton = document.getElementById('incrementBtn');
+const resetButton = document.getElementById('resetBtn');
+
+incrementButton.addEventListener('click', function() {
+  count++;
+  counterDisplay.textContent = count;
+});
+resetButton.addEventListener('click', function() {
+  count = 0;
+  counterDisplay.textContent = count;
+})
